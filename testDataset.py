fcf9498a3d0941b49a3cbe90e2834908ef6358cc
LeCongThuong <thuongle276@gmail.com>
2024-12-23 19:03:09 +0700

    add run test data

--- testDataset.py
+++ testDataset.py
@@ -3,6 +3,8 @@ import pandas as pd
 import os
 from PIL import Image
 from torchvision.transforms import Compose, ToTensor
+from torch.utils.data import DataLoader
+
 
 
 class TestDataset(nn.Module):
@@ -10,7 +12,7 @@ class TestDataset(nn.Module):
         self.root_data = root_data
         self.transform = transform
         self.csv_file = csv_file
-        data_info = pd.read_csv(self.csv_file, header=None)
+        self.data_info = pd.read_csv(self.csv_file, header=None)
 
 
     def __len__(self):
@@ -21,17 +23,20 @@ class TestDataset(nn.Module):
         image = Image.open(img_name)
         image = self.transform(image)
         return {"image": image, "name": self.data_info.iloc[idx, 0]}
+
     
 
 
 if __name__ == '__main__':
-    root_data = 'data/cropped'
-    csv_file = 'data/test.csv'
+    root_data = '/mnt/hmi/thuong/Photoface_dist/PhotofaceDBNormal'
+    csv_file = '/mnt/hmi/thuong/Photoface_dist/PhotofaceDBNormalTrainValTest/dataset_0/test.csv'
 
     transform = Compose([
         ToTensor()
     ])
 
     test_dataset = TestDataset(root_data, csv_file, transform)
-    print(test_dataset[0])
-    print(len(test_dataset))
+    test_dl = DataLoader(test_dataset, batch_size=32, shuffle=False, drop_last=False)
+    for idx, data in enumerate(test_dl):
+        print(data["image"].shape, data["name"][0])
+        break
--- tester.py
+++ tester.py
@@ -5,25 +5,66 @@ from resnet_unet.resnet_unet_model import ResNetUNet
 import cv2
 import numpy as np
 from PIL import Image
+from testDataset import TestDataset
+from torch.utils.data import DataLoader
+from dataclasses import dataclass
+import os
+from pathlib import Path
+from tqdm import tqdm
 
-img_transform = Compose([
-  ToTensor()
-])
+@dataclass
+class Config:
+    data_dir: str = "/mnt/hmi/thuong/Photoface_dist/PhotofaceDBNormal"
+    csv_data_path: str = "/mnt/hmi/thuong/Photoface_dist/PhotofaceDBNormalTrainValTest/dataset_0/test.csv"
+    dest_dir: str = "outputs"
+    chkpt_path: str = "data/model.pth"
+    batch_size: int = 32
 
-model = ResNetUNet(n_class = 3).cuda()
-model.load_state_dict(torch.load('data/model.pth'))
-                                               
-model.eval()
-      
-img = img_transform(Image.open('data/cropped/0.jpg')).unsqueeze(0)  
-img = Variable(img.cuda())
-  
-outs = model(img)[0]  
-out = np.array(outs[0].data.permute(1,2,0).cpu())  
-out = out / np.expand_dims(np.sqrt(np.sum(out * out, 2)),2)
-out = 127.5 * (out + 1.0)
 
-cv2.imwrite('data/output/0.jpg', cv2.cvtColor(out, cv2.COLOR_RGB2BGR))
+class TestProcessing:
+  def __init__(self, cfg):
+    self.cfg = cfg
+    img_transform = Compose([
+                          ToTensor()
+                        ])
+    self.model = self._init_model(cfg.chkpt_path)
+    self.test_dataset = TestDataset(cfg.data_dir, cfg.csv_data_path, img_transform)
+    
+    
+  def save_imgs(self, model_output, data_paths, epsilon = 1e-9):
+    model_output_perm = model_output.permute(0, 2, 3, 1)
+    model_output_np = model_output_perm.cpu().numpy() 
+    norms = np.linalg.norm(model_output_np, axis=-1, keepdims=True)  # shape: (BS, H, W, 1)
+    # Add a small epsilon to avoid division by zero
+    normal_vectors_np = model_output_np / (norms + epsilon)
+
+    num_img = model_output.shape[0]
+    for i in range(num_img):
+      data_path = data_paths[i]
+      normal_v = normal_vectors_np[i]
+      dest_path = os.path.join(cfg.dest_dir, str(data_path).replace("crop.jpg", "predict.npy"))
+      Path(dest_path).parent.mkdir(parents=True, exist_ok=True)
+      np.save(dest_path, normal_v)
+ 
+    
+  def _init_model(self, chkpt_path):
+    model = ResNetUNet(n_class = 3).cuda()
+    model.load_state_dict(torch.load(chkpt_path))
+    model.eval()
+    return model 
+
+  def process(self):
+    test_dl = DataLoader(self.test_dataset, batch_size=cfg.batch_size, shuffle=False, drop_last=False)
+    with torch.no_grad():
+      for idx, data in tqdm(enumerate(test_dl)):
+        img, img_paths = data["image"].cuda(), data["name"]
+        out = self.model(img)[0]
+        self.save_imgs(out, img_paths)
+
+if __name__ == "__main__":
+  cfg  = Config()
+  test_process = TestProcessing(cfg)
+  test_process.process()
   
 
 
